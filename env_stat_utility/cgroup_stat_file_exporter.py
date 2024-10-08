f0b85793f7eac9beffa96bbc70a3cd372e0b5da1
Bayarto Neltanov <bayarto2003@gmail.com>
2024-10-08 15:42:16 +0700

    12257: Переименовал requested_stats внутри классов

--- env_stat_utility/cgroup_stat_file_exporter.py
+++ env_stat_utility/cgroup_stat_file_exporter.py
@@ -2,18 +2,18 @@ import json
 
 
 class CgroupStatFileExporter:
-    def __init__(self, cgroup_stats, stat_metrics):
-        self.stat_metrics = stat_metrics
+    def __init__(self, cgroup_stats, requested_stats):
+        self.requested_stats = requested_stats
         self.cgroup_stats = cgroup_stats
 
     def save_to_json(self, filename):
         try:
             stats_list = {}
-            if self.stat_metrics['cpu']:
+            if self.requested_stats['cpu']:
                 stats_list['cpu_usage'] = (self.cgroup_stats.get_cpu_usage())
-            if self.stat_metrics['memory']:
+            if self.requested_stats['memory']:
                 stats_list['memory_usage'] = (self.cgroup_stats.get_memory_usage())
-            if self.stat_metrics['disk']:
+            if self.requested_stats['disk']:
                 disk_usage_list = {"io_stat": self.cgroup_stats.get_io_stat(),
                                    "io_pressure": self.cgroup_stats.get_io_pressure()}
                 stats_list['disk_usage'] = disk_usage_list
