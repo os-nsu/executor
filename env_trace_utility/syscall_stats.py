0f33e036f22930f163c4a3c35a6347faf2f843af
Bayarto Neltanov <bayarto2003@gmail.com>
2024-10-19 13:13:41 +0700

    12378: убрал зависимость между cgroup_stats и syscall_stats

--- env_trace_utility/syscall_stats.py
+++ env_trace_utility/syscall_stats.py
@@ -9,15 +9,30 @@ class SyscallStats:
         self.name = name
 
     def __trace_pid(self, pid):
-        ec = os.system(f"strace -f -tt -C -S calls -U name,calls -o traces{self.name}_trace -p {pid}")
+        os.system("mkdir traces")
+        ec = os.system(f"strace -f -tt -C -S calls -U name,calls -o traces/{self.name}_trace -p {pid}")
         if ec != 0:
             print(f"Failed to trace pid: {pid}")
+
+    def get_cgroup_pids(self):
+        try:
+            with open(f'/sys/fs/cgroup/{self.name}/cgroup.procs', 'r') as f:
+                cgroup_pids = []
+                for line in f:
+                    cgroup_pids.append(line.strip())
+                return cgroup_pids
+        except FileNotFoundError:
+            print(f"cgroup.procs file not found for cgroup {self.name}")
+            return None
+        except Exception as e:
+            print(f"Error getting cgroup pids: {e}")
+            return None
 
     def get_syscall_stats(self, requested_pid):
         if requested_pid is not None:
             self.__trace_pid(requested_pid)
         else:
-            traced_pids = CgroupStats(self.name).get_cgroup_pids()
+            traced_pids = self.get_cgroup_pids()
             threads = []
             for pid in traced_pids:
                 thread = threading.Thread(target=self.__trace_pid, args=(pid,))
