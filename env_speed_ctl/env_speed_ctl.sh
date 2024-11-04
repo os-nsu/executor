55c32da3c769f72680b86727bc09266dca7222bc
Tofig Aliev <t.aliev@postgrespro.ru>
2024-11-04 13:04:46 +0700

    Add review fixes

--- env_speed_ctl/env_speed_ctl.sh
+++ env_speed_ctl/env_speed_ctl.sh
@@ -1,46 +1,129 @@
 #!/bin/bash  
 
 TC=/sbin/tc
+IP=/sbin/ip
+MODPROBE=/sbin/modprobe
 
-INTERFACE=wlp0s20f3
-INTERFACE_SPEED=100mbit
-UPLOAD_SPEED_LIMIT=400kbps
-DOWNLOAD_SPEED_LIMIT=400kbps
-LIMIT_APPLY_TO=0.0.0.0
+INTERFACE="";
+UPLOAD_SPEED_LIMIT="";
+DOWNLOAD_SPEED_LIMIT="";
+LIMIT_APPLY_TO=0.0.0.0;
+ACTION="add_rules";
+IFB="ifb0";
 
 function remove_rules {
-    $TC qdisc del dev $INTERFACE root
+    if [[ -z $INTERFACE ]]; then
+        echo "interface name is required";
+        exit 1;
+    fi
+
+    $TC qdisc del dev $INTERFACE   root 2>/dev/null;
+    $TC qdisc del dev $IFB         root 2>/dev/null;
+    $TC qdisc del dev $INTERFACE   ingress 2>/dev/null;
+    $TC qdisc del dev $IFB         ingress 2>/dev/null;
 }
 
-function add_rules { 
-    remove_rules
+function add_rules {
+    if [[ -z $INTERFACE ]]; then
+        echo "interface name is required";
+        exit 1;
+    fi
 
-    $TC qdisc add dev $INTERFACE root handle 1:0 htb default 10
-    $TC class add dev $INTERFACE parent 1:0 classid 1:1 htb rate $INTERFACE_SPEED
-    $TC class add dev $INTERFACE parent 1:1 classid 1:10 htb rate 5mbit ceil $UPLOAD_SPEED_LIMIT
-    $TC class add dev $INTERFACE parent 1:1 classid 1:30 htb rate 5mbit ceil $DOWNLOAD_SPEED_LIMIT
+    if [[ -z $DOWNLOAD_SPEED_LIMIT ]]; then
+        echo "download speed limit is required";
+        exit 1;
+    fi
 
-    $TC filter add dev $INTERFACE protocol ip parent 1:0 prio 2 u32 match ip dst $LIMIT_APPLY_TO/32 flowid 1:10
-    $TC filter add dev $INTERFACE protocol ip parent 1:0 prio 1 u32 match ip dst $LIMIT_APPLY_TO/32 flowid 1:30
+    if [[ -z $UPLOAD_SPEED_LIMIT ]]; then
+        echo "upload speed limit is required";
+        exit 1;
+    fi
+
+    remove_rules;
+
+    $TC qdisc add dev $INTERFACE root handle 1: htb default 20;
+    $TC class add dev $INTERFACE parent 1: classid 1:1 htb rate "${UPLOAD_SPEED_LIMIT}kbit" prio 5;
+
+    RATE=$((50*${UPLOAD_SPEED_LIMIT}/100))
+    if [ "$RATE" -eq 0 ]; then
+        RATE=1;
+    fi
+
+    $TC class add dev $INTERFACE parent 1:1 classid 1:10 htb \
+        rate "${RATE}kbit" ceil "${UPLOAD_SPEED_LIMIT}kbit" prio 1;
+
+    RATE=$((50*${UPLOAD_SPEED_LIMIT}/100))
+    if [ "$RATE" -eq 0 ]; then
+        RATE=1;
+    fi
+
+    $TC class add dev $INTERFACE parent 1:1 classid 1:20 htb \
+        rate "${RATE}kbit" ceil "${UPLOAD_SPEED_LIMIT}kbit" prio 2;
+
+    $MODPROBE ifb numifbs=1;
+    $IP link set dev $IFB up;
+
+    $TC qdisc add dev $INTERFACE handle ffff: ingress;
+    $TC filter add dev $INTERFACE parent ffff: protocol ip u32 match u32 0 0 \
+        action mirred egress redirect dev $IFB;
+
+    $TC qdisc add dev $IFB root handle 2: htb;
+    $TC class add dev $IFB parent 2: classid 2:1 htb rate "${DOWNLOAD_SPEED_LIMIT}kbit";
+
+    $TC filter add dev $IFB protocol ip parent 2: prio 1 u32 match ip src $LIMIT_APPLY_TO/0 flowid 2:1;
 }
 
 function check_rules {
-    $TC -s qdisc ls dev $INTERFACE
+    $TC -s qdisc ls dev $INTERFACE;
 }
 
 function print_usage {
-    echo "USAGE: speed_ctl [OPTION]"
-    echo "add_rules - add limit rules for network interface"
-    echo "remove_rules - remove rules"
-    echo "check_rules - show rules"
+    echo "$0 [-hc] [-a <adapter>] [-d <rate>] [-u <rate>]";
+    echo -e "-h  help";
+    echo -e "-c  remote rules";
+    echo -e "-a  interface name";
+    echo -e "-d  download speed rate";
+    echo -e "-u  upload speed rate";
 }
 
-if [ -z "$1" ]; then
-    print_usage
-elif [ "$1" == "add_rules" ]; then
-    add_rules
-elif [ "$1" == "remove_rules" ]; then
-    remove_rules
-elif [ "$1" == "check_rules" ]; then
-    check_rules
+if [ $# -eq 0 ]; then
+    print_usage;
+    exit 1;
+fi
+
+while getopts ":hca:u:d:" opt; do
+    case $opt in
+        h)
+            ACTION="print_usage"
+            ;;
+        c)
+            ACTION="remove_rules"
+            ;;
+        a)
+            INTERFACE="$OPTARG"
+            ;;
+        d)
+            DOWNLOAD_SPEED_LIMIT="$OPTARG"
+            ;;
+        u)
+            UPLOAD_SPEED_LIMIT="$OPTARG"
+            ;;
+        :)
+            echo "Option -$OPTARG argument required." >&2
+            exit 1
+            ;;
+        ?)
+            echo "incorrect param: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+
+
+if [ "$ACTION" == "add_rules" ]; then
+    add_rules;
+elif [ "$ACTION" == "remove_rules" ]; then
+    remove_rules;
+else
+    print_usage;
 fi
