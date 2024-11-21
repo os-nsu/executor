2a1379f03296a70b78eb493143cab5f40fc2881a
Bayarto Neltanov <bayarto2003@gmail.com>
2024-11-21 19:29:47 +0700

    12504: Add network interface name as command line argument

--- env_stat_utility/env-get-stats.py
+++ env_stat_utility/env-get-stats.py
@@ -13,11 +13,15 @@ def main():
     parser.add_argument('--memory', action='store_true', help='RAM usage')
     parser.add_argument('--disk', action='store_true', help='disk usage')
     parser.add_argument('--network', action='store_true', help='network usage (bytes transmitted and received)')
+    parser.add_argument('--interface_name', type=str, help='virtual network interface name')
     parser.add_argument('-w', '--watch', action='store_true', help='watch mode')
     parser.add_argument('-o', '--output', type=str, default='stats.json', help='output file (.json)')
     args = parser.parse_args()
 
-    cgroup_stats = CgroupStats(args.env_name)
+    if (args.network and not args.interface_name) or (args.interface_name and not args.network):
+        parser.error(f"Error: the arguments '--network' and '--interface_name' must be specified together")
+
+    cgroup_stats = CgroupStats(args.env_name, args.interface_name)
 
     requested_stats = {key: value for key, value in vars(args).items() if key in ['cpu', 'memory', 'disk', 'network']}
     if True in requested_stats.values():
