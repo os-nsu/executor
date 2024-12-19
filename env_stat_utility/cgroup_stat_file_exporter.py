f55bfe3cbbeb2429e7fdf90a52daae3ea808ab70
Neltanov Bayarto <93517386+neltanov@users.noreply.github.com>
2024-12-19 18:55:26 +0700

    13710: Refactor code of env-get-stats.py script. (#10)
    - moved the argument parsing to a separate function
    - now CgroupStatFileExporter accepts already collected statistics in the form of a dictionary and outputs it to a file in json format

--- env_stat_utility/cgroup_stat_file_exporter.py
+++ env_stat_utility/cgroup_stat_file_exporter.py
@@ -2,25 +2,11 @@ import json
 
 
 class CgroupStatFileExporter:
-    def __init__(self, cgroup_stats, requested_stats):
-        self.requested_stats = requested_stats
-        self.cgroup_stats = cgroup_stats
-
-    def save_to_json(self, filename):
+    @staticmethod
+    def save_dict_to_json(cgroup_stats_dictionary, cgroup_stat_output_filename):
         try:
-            stats_dict = {}
-            if self.requested_stats['cpu']:
-                stats_dict['cpu_usage'] = self.cgroup_stats.get_cpu_usage()
-            if self.requested_stats['memory']:
-                stats_dict['memory_usage'] = self.cgroup_stats.get_memory_usage()
-            if self.requested_stats['disk']:
-                disk_usage_dict = {"io_stat": self.cgroup_stats.get_io_stat(),
-                                   "io_pressure": self.cgroup_stats.get_io_pressure()}
-                stats_dict['disk_usage'] = disk_usage_dict
-            if self.requested_stats['network']:
-                stats_dict['network_usage'] = self.cgroup_stats.get_network_stat()
-            with open(filename, 'w+') as f:
-                f.write(json.dumps(stats_dict, indent='\t'))
+            with open(cgroup_stat_output_filename, 'w+') as f:
+                f.write(json.dumps(cgroup_stats_dictionary, indent='\t'))
         except FileExistsError as e:
             print(f'File not found: {e}')
         except PermissionError as e:
--- env_stat_utility/env-get-stats.py
+++ env_stat_utility/env-get-stats.py
@@ -5,7 +5,7 @@ from curses_cgroup_stat_printer import CursesCgroupStatPrinter
 from cgroup_stat_file_exporter import CgroupStatFileExporter
 
 
-def main():
+def parse_arguments():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--env_name', type=str, required=True, help='environment name')
@@ -21,6 +21,29 @@ def main():
     if not args.interface_name:
         parser.error(f"Error: the argument '--interface_name' is required")
 
+    return args
+
+
+def collect_cgroup_stats(cgroup_stats, requested_stats):
+    stats_dict = {}
+
+    if requested_stats['cpu']:
+        stats_dict['cpu_usage'] = cgroup_stats.get_cpu_usage()
+    if requested_stats['memory']:
+        stats_dict['memory_usage'] = cgroup_stats.get_memory_usage()
+    if requested_stats['disk']:
+        disk_usage_dict = {"io_stat": cgroup_stats.get_io_stat(),
+                           "io_pressure": cgroup_stats.get_io_pressure()}
+        stats_dict['disk_usage'] = disk_usage_dict
+    if requested_stats['network']:
+        stats_dict['network_usage'] = cgroup_stats.get_network_stat()
+
+    return stats_dict
+
+
+def main():
+    args = parse_arguments()
+
     cgroup_stats = CgroupStats(args.env_name, args.interface_name)
 
     requested_stats = {key: value for key, value in vars(args).items() if key in ['cpu', 'memory', 'disk', 'network']}
@@ -29,9 +52,10 @@ def main():
             printer = CursesCgroupStatPrinter(cgroup_stats, requested_stats)
             printer.show_data()
         else:
+            stats_dict = collect_cgroup_stats(cgroup_stats, requested_stats)
             output_filename = args.output
-            json_builder = CgroupStatFileExporter(cgroup_stats, requested_stats)
-            json_builder.save_to_json(output_filename)
+
+            CgroupStatFileExporter.save_dict_to_json(stats_dict, output_filename)
             print(f"{cgroup_stats.name} statistics have been saved to a file {output_filename}")
     else:
         print("No stats requested")
